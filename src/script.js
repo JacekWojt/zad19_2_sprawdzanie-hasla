d3fe804e402b9f55338c52293f4ca24563b0d5c8
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-03-30 20:32:29 +0200

    Poprawka 1

--- src/script.js
+++ src/script.js
@@ -1,40 +1,42 @@
-function check(){
+function check() {
     let input = document.getElementById("password").value;
-    if(input.length != 0) {
+    if (input.length !== 0) {
         document.getElementById("validation").style.display = "block";
         document.getElementById("diff-passwords").style.display = "block";
     }
-    if(input.match(/[a-z]/)){
+    if (input.match(/[a-z]/)) {
         document.getElementById("one-small").style.display = "none";
-    }else{
-        document.getElementById("one-small").style.display = "block";
-        document.getElementById("one-small").style.color = "red";
+    } else {
+        showElement(document.getElementById("one-small"));
     }
-    if(input.match(/[A-Z]/)){
+    if (input.match(/[A-Z]/)) {
         document.getElementById("one-capital").style.display = "none";
-    }else{
-        document.getElementById("one-capital").style.display = "block";
-        document.getElementById("one-capital").style.color = "red";
+    } else {
+        showElement(document.getElementById("one-capital"));
     }
     if(input.length >= 8) {
         document.getElementById("length").style.display = "none";
-    }else{
-        document.getElementById("length").style.display = "block";
-        document.getElementById("length").style.color = "red";
+    } else {
+        showElement(document.getElementById("length"));
     }
-    if(input.match(/[^A-Za-z0-9-'']/i)){
+    if (input.match(/[^A-Za-z0-9-'']/i)) {
         document.getElementById("one-special").style.display = "none";
-    }else{
-        document.getElementById("one-special").style.display = "block";
-        document.getElementById("one-special").style.color = "red";
+    } else {
+        showElement(document.getElementById("one-special"));
     }
 }
+
+function showElement(element) {
+    element.style.display = "block";
+    element.style.display = "red";
+}
+
 function confirm() {
     let password = document.getElementById("password");
     let confirm = document.getElementById("confirm-password");
-    if(password.value == confirm.value) {
+    if (password.value === confirm.value) {
         document.getElementById("diff-passwords").style.display = "none";
-    }else{
+    } else {
         document.getElementById("diff-passwords").style.color = "red";
     }
 }
